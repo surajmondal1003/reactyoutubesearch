a1704d35cf3070d90ae12a90b4da5c5676bb761d
surajmondal1003 <surajmondal1003@gmail.com>
2018-06-22 00:33:46 +0530

    dynamic search term

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,39 +1,27 @@
-import React,{ Component } from 'react';
+import React, { Component } from "react";
 
-//functional based component
-// const SearchBar = () =>{
-//     return <input/>;
-// };
-//class based component
-
-class SearchBar extends Component{
-constructor(props){
+class SearchBar extends Component {
+  constructor(props) {
     super(props);
 
-    this.state={ term:'' };
-}
+    this.state = { term: "" };
+  }
 
-    // render(){
-    //     return <input onChange={this.onInputChange}/>;
-    // }
+  render() {
+    return (
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={event => this.onInputChange(event.target.value)}
+        />
+      </div>
+    );
+  }
 
-    //using ES6 arrow function
-    // render(){
-    //     return <input onChange={(event)=>console.log(event.target.value)}/>;
-    // }
-
-    render(){
-        return (
-            <div>
-                <input value = {this.state.term} onChange={this.onInputChange} />
-            </div>
-        );
-    }
-    onInputChange = (event) => {
-        this.setState({term: event.target.value});
-        this.props.onSearch(event.target.value)
-        console.log(event.target.value);
-    }
+  onInputChange(term) {
+    this.setState({ term });
+    this.props.onSearchTermChange(term);
+  }
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,45 +1,49 @@
-import React , { Component } from 'react';
-import ReactDOM from 'react-dom';
-import SearchBar from './components/searchbar';
-import YTSearch from 'youtube-api-search';
-import VideoList from './components/video_list';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-const API_KEY='AIzaSyCoZQroz6O4BpS2C12_lVGOOqSA_Rv1OmY';
+import _ from "lodash";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+import SearchBar from "./components/searchbar";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
+const API_KEY = "AIzaSyCoZQroz6O4BpS2C12_lVGOOqSA_Rv1OmY";
 
+class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
+    this.videoSearch("surfboards");
+  }
 
-class App extends Component{
-    //return <div>Hi !! Welcome To React</div>
-    constructor(props){
-        super(props);
-        this.state={ videos:[] };
-    }
-    
-    componentWillMount() {
-        YTSearch({key : API_KEY, term : 'surfboards'} , (videos) => {
-            this.setState({ videos });
-            //console.log(videos);
-        });
-    }
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
 
-    onSearch = term => {
-        console.log(term);
-        YTSearch({key : API_KEY, term} , videos => {
-            this.setState({ videos });
-            console.log(videos);
-        });
-    }
+  render() {
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 300);
 
-    render(){
-        return (<div>
-                    <SearchBar onSearch={this.onSearch}/>
-                    <VideoList videos={this.state.videos}/>
-                </div>
-            );
-    }
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.querySelector(".container"));
